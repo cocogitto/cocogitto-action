d0b7ffdfa523a071ee64b591614f123a1f1242cd
Vincent Tavernier <vtavernier@users.noreply.github.com>
2023-12-26 01:41:26 +0100

    feat: update cocogitto to 6.0.1

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 CUR_DIR=$(pwd)
-VERSION=5.6.0
+VERSION=6.0.1
 TAR="cocogitto-$VERSION-x86_64-unknown-linux-musl.tar.gz"
 BIN_DIR="$HOME/.local/bin"
 
 mkdir -p "$BIN_DIR"
 cd "$BIN_DIR" || exit
 curl -OL https://github.com/cocogitto/cocogitto/releases/download/"$VERSION"/"$TAR"
-tar xfz $TAR
+tar --strip-components=1 -xzf $TAR x86_64-unknown-linux-musl/cog
 cd "$CUR_DIR" || exit
