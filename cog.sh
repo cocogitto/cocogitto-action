e598e4390eb323067f17f4f4fcc71d7c7497941b
Mark S <the@wondersmith.dev>
2024-05-14 11:09:06 -0400

    feat: enable use of `cog verify` in action
    Signed-off-by: Mark S <the@wondersmith.dev>

--- cog.sh
+++ cog.sh
@@ -2,28 +2,29 @@
 
 set -a
 
-CHECK=$1
-LATEST_TAG_ONLY=$2
-RELEASE=$3
-GIT_USER=$4
-GIT_USER_EMAIL=$5
+CHECK="${1}"
+LATEST_TAG_ONLY="${2}"
+RELEASE="${3}"
+GIT_USER="${4}"
+GIT_USER_EMAIL="${5}"
+VERIFY="${7}"
 
-echo "Setting git user : $GIT_USER"
-git config --global user.name "$GIT_USER"
+echo "Setting git user : ${GIT_USER}"
+git config --global user.name "${GIT_USER}"
 
-echo "Settings git user email $GIT_USER_EMAIL"
-git config --global user.email "$GIT_USER_EMAIL"
+echo "Settings git user email ${GIT_USER_EMAIL}"
+git config --global user.email "${GIT_USER_EMAIL}"
 
 cog --version
 
-if [ "$CHECK" = "true" ]; then
-  if [ "$LATEST_TAG_ONLY" = "true" ]; then
+if [ "${CHECK}" = "true" ]; then
+  if [ "${LATEST_TAG_ONLY}" = "true" ]; then
     if [ "$(git describe --tags --abbrev=0)" ]; then
       message="Checking commits from $(git describe --tags --abbrev=0)"
     else
       message="No tag found checking history from first commit"
     fi
-    echo "$message"
+    echo "${message}"
     cog check --from-latest-tag || exit 1
   else
     echo "Checking all commits"
@@ -31,8 +32,12 @@ if [ "$CHECK" = "true" ]; then
   fi
 fi
 
-if [ "$RELEASE" = "true" ]; then
+if [ "${RELEASE}" = "true" ]; then
   cog bump --auto || exit 1
   VERSION="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
   echo "version=$VERSION" >> $GITHUB_OUTPUT
+fi
+
+if ( echo "${VERIFY}" | grep -Eiv '^([01]|(true)|(false))$' > /dev/null ) ; then
+  cog verify "${VERIFY}" || exit 1
 fi
